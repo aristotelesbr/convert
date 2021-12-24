adfcc506ec917d1412c5c74bb01c9287e396416d
Aristóteles Coutinho <aristotelesbr@gmail.com>
2021-12-24 10:16:33 -0300

    feat: added values-in fun

--- src/converter/core.clj
+++ src/converter/core.clj
@@ -1,7 +1,16 @@
 (ns converter.core
   (:gen-class))
 
+(defn- values-in [arg]
+  (cond
+    (.startsWith arg "--from=")
+    {:from (.substring arg 7)}
+    (.startsWith arg "--to")
+    {:to (.substring arg 5)}
+    :else {}))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "we have" (count args) "args")
+  (println "They are: " (map values-in args)))
