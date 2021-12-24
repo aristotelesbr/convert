49ac14fc34ae1d8f84b63aa469ee47d39e73d601
Aristóteles Coutinho <aristotelesbr@gmail.com>
2021-12-24 19:57:37 -0300

    feat: added clj-http and config api request

--- src/converter/core.clj
+++ src/converter/core.clj
@@ -1,6 +1,17 @@
 (ns converter.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clj-http.client :as http-client])
   (:gen-class))
+
+(def api-secret-key "")
+
+(def api-url
+  "https://free.currencyconverterapi.com/api/v6/convert")
+
+(defn parameterize-currency
+  "Given two args, A and B must be parameterize to return A_B"
+  [from to]
+  (str from "_" to))
 
 (def options
   [["-f" "--from currency base" "currency base to converter" :default "eur"]
@@ -9,6 +20,9 @@
 (defn -main
   "app bootstrap!"
   [& args]
-  (println "we have" (count args) "args")
-  (println "They are: " (:options
-                         (parse-opts args options))))
+  (let [{:keys [from to]} (:options
+                           (parse-opts args options))]
+
+    (prn "Quote: " (http-client/get api-url
+                                    {:query-params {"q" (parameterize-currency from to)
+                                                    "apiKey" api-secret-key}}))))
