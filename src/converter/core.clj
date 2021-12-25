63c06e64f51aaee7b36d25a5edb91075704235ad
aristotelesbr <aristotelesbr@gmail.com>
2021-12-25 09:36:39 -0300

    feat: added get-quote fun

--- src/converter/core.clj
+++ src/converter/core.clj
@@ -1,8 +1,13 @@
 (ns converter.core
   (:require [clojure.tools.cli :refer [parse-opts]]
+            [cheshire.core :refer [parse-string]]
             [clj-http.client :as http-client]
             [dotenv :refer [env]])
   (:gen-class))
+
+(def options
+  [["-f" "--from currency base" "currency base to converter" :default "eur"]
+   ["-t" "--to destination currency" "currency we want to know the value"]])
 
 (def api-secret-key  (env :API_KEY))
 
@@ -10,20 +15,25 @@
   "https://free.currencyconverterapi.com/api/v6/convert")
 
 (defn parameterize-currency
-  "Given two args, A and B must be parameterize to return A_B"
+  "Given two args, A and B parameterize must be return 'A_B' data"
   [from to]
   (str from "_" to))
 
-(def options
-  [["-f" "--from currency base" "currency base to converter" :default "eur"]
-   ["-t" "--to destination currency" "currency we want to know the value"]])
+(defn get-quote [from to]
+           (let [currency (parameterize-currency from to)]
+             (-> (:body (http-client/get api-url
+                                         {:query-params {"q" currency "apiKey" api-secret-key}}))
+                 (parse-string)
+                 (get-in ["results" currency "val"]))))
+
+(defn- format-output [quote from to]
+  (str "1 " from " equals to " quote " in " to))
 
 (defn -main
   "app bootstrap!"
   [& args]
   (let [{:keys [from to]} (:options
                            (parse-opts args options))]
-
-    (prn "Quote: " (http-client/get api-url
-                                    {:query-params {"q" (parameterize-currency from to)
-                                                    "apiKey" api-secret-key}}))))
+    (-> (get-quote from to)
+        (format-output from to)
+        (prn))))
